0102c27bcb528f756f3a94ebf9e86ffe67af7b01
Daniel Fritsi <ffddani@gmail.com>
2022-09-09 20:26:37 +0200

    Initial commit

---
+++ src/fritsi/py/orm/__init__.py
@@ -0,0 +1,12 @@
+"""
+A Python ORM library.
+"""
+
+__version__ = "0.1"
+
+__author__ = "Daniel Fritsi"
+__author_email__ = "ffddani@gmail.com"
+
+__license__ = "BSD-3-Clause"
+
+__url__ = "https://github.com/fritsi/py-orm"
